a64f591986d40362490825e1dc1df76428b0ad02
ValentinHG321 <dart.veidar1994@abv.bg>
2022-03-29 18:59:00 +0300

    SMT machine P&P file converter to useful format.

---
+++ PickAndPlace/PickAndPlace/SMD.cs
@@ -0,0 +1,29 @@
+﻿namespace PickAndPlace
+{
+    public class SMD
+    {
+        public SMD(string designator, string package, string xAxis, string yAxis, string rotation, string partName)
+        {
+            Designator = designator;
+            Package = package;
+            XAxis = xAxis;
+            YAxis = yAxis;
+            Rotation = rotation;
+            PartName = partName;
+        }
+        public string Designator { get; set; }
+        public string Package { get; set; }
+        public string XAxis { get; set; }
+        public string YAxis { get; set; }
+        public string Rotation { get; set; }
+        public string PartName { get; set; }
+
+        public override string ToString()
+        {
+            string output = $"\"{this.PartName} {this.Package}\" {this.Designator} {this.XAxis} {this.YAxis} {this.Rotation}";
+            return output;
+        }
+    }
+}
+
+
